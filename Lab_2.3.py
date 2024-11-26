ca682719cfae9ac9c0e0fea7193fe2a15a11398f
ParhamWolf3000 <parham.jobb@gmail.com>
2024-11-26 23:25:54 +0100

    Lab_2.3

---
+++ Lab_2.3.py
@@ -0,0 +1,13 @@
+"""
+Create a program that prints a chosen measure in meters and
+prints the value in kilometers and in centimeters.
+
+You can assume that the value will not be negative values.
+"""
+
+meters = 100
+
+kilometers = meters / 1000
+centimeters = meters * 100
+
+print(f"{kilometers} km = {meters} m = {centimeters} cm")
