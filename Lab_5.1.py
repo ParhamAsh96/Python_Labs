4c2d70e7a74c991e2b2f0b1b9eca407519892fac
ParhamWolf3000 <parham.jobb@gmail.com>
2024-12-09 00:39:23 +0100

    Lab_5.1

---
+++ Lab_5.1.py
@@ -0,0 +1,17 @@
+"""
+• Ask the user to enter a number
+• Create a multiplication table (from 1 to 10) of the number.
+
+• For example, for number 5:
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+...
+5 x 10 = 50
+"""
+
+number = int(input("Please enter a number: "))
+
+for i in range(1, 11):
+    result = number * i
+    print(f"{number} x {i} = {result}")
